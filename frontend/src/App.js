e1249f2b324463d06ac1f4f7b3d1a114aff03465
David Daniels <dldaniels07@yahoo.com>
2022-08-20 21:27:38 -0600

    landing page,nav bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
-function App() {
+const App = () => {
   return (
     <>
-      <h1>Awesome Builder 3</h1>
+    <Header />
+    <main className='py-3'>
+      <Container>
+      <h1>Dinosaur Toybox</h1>
+      </Container>
+      <Footer />
+    </main> 
+     
     </>
   );
 }
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+const Footer = () => {
+  return (<footer>
+    <main>
+    <Container>
+        <Row>
+            <Col className='text-center py-3'>Copyright &copy; Dinosaur Toybox 2022</Col>
+        </Row>
+    </Container>
+    </main>
+    </footer>
+  )
+}
+
+export default Footer
